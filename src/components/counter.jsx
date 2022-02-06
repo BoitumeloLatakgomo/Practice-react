52de65c6097613f7c8c7aa3ac1b64d0f8b9c6ce1
BOITUMELO LATAKGOMO <201736639@keyaka.ul.ac.za>
2022-02-06 17:08:51 +0200

    pushed files

---
+++ src/components/counter.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+ class Counter extends Component {
+
+    state = {
+        count: 0
+       
+    };
+    
+    handleIncrement = () => {
+        console.log("test");
+        this.setState({count: this.state.count + 1})
+    };
+   
+    render() { 
+        
+        console.log("test");
+        return(
+        <div>
+             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+             <button 
+             onClick={this.handleIncrement}
+             className="btn btn-secondary btn-sm">
+                 Increment
+                 </button>
+             
+        </div>
+        );
+        
+    }
+     getBadgeClasses() {
+         let classes = "badge m-3 badge-";
+         classes += this.state.count === 0 ? "warning" : "primary";
+         return classes;
+     }
+
+    formatCount(){ 
+        const {count} = this.state;
+        return count === 0 ? "On zero" : count;
+    }
+}
+
+ export default Counter;
